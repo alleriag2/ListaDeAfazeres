417af76ab24793f2091f78db6d92e1fadb0d235b
Karla Félix <karlakaroline_7@hotmail.com>
2020-01-05 21:38:24 -0400

    Home_page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Todo, ToDoService } from './../services/to-do.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit {
+
+  todos: Todo[];
+
+  constructor(private todoService: ToDoService) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.todoService.getTodos().subscribe(res => {
+      this.todos = res;
+    });
+  }
+
+  remove(item) {
+    this.todoService.removeTodo(item.id);
+  }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDyt15sP61NKw2-Z4GpRuEcyDLfHNhsiq4",
+    authDomain: "chat-29ea9.firebaseapp.com",
+    databaseURL: "https://chat-29ea9.firebaseio.com",
+    projectId: "chat-29ea9",
+    storageBucket: "chat-29ea9.appspot.com",
+    messagingSenderId: "419653777660",
+    appId: "1:419653777660:web:06999247064b7f999c7f79",
+    measurementId: "G-CGM4TW2258"
+  }
 };
